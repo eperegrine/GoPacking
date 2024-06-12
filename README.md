5a8b5c84344d1cde83f4b7bd8f3754fd97868bc0
Emily Peregrine <emily.peregrine1@nhs.net>
2024-06-12 21:42:00 +0100

    Added server

---
+++ README.md
@@ -0,0 +1,7 @@
+# Packing Manager
+
+A tool written in go to split an order down in the most efficient way
+
+## How to use
+
+Set the query parameter `quantity` to be the amount you want to manage
--- index.go
+++ index.go
@@ -1,42 +1,70 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"log"
+	"net/http"
 	"strconv"
 	"strings"
 )
 
-func main() {
+func packHandler(w http.ResponseWriter, r *http.Request) {
 	availablePacks := []int{5000, 2000, 1000, 500, 250}
-	fmt.Println("Welcome to the stock packer: ")
-	fmt.Print("Available pack sizes: ")
-	for _, pack := range availablePacks {
-		fmt.Print(pack)
-		fmt.Print(", ")
-	}
-	fmt.Println()
-	fmt.Println("To exit enter 'q'")
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Enter number of items to pack: ")
-		text, _ := reader.ReadString('\n')
-		text = strings.TrimSpace(text)
 
-		if text == "q" {
-			break
-		}
-		// string to int
-		qty, err := strconv.Atoi(text)
-		if err != nil {
-			panic(err)
-		}
-		packing := packItems(availablePacks, qty)
+	qtyStr := r.URL.Query().Get("quantity")
 
-		for _, entry := range packing {
-			fmt.Printf("%d x %d items\n", entry.quantity, entry.pack_volume)
+	if r.URL.Query().Has("packs") {
+		packsStr := r.URL.Query().Get("packs")
+		packsAsStrings := strings.Split(packsStr, ",")
+		availablePacks = []int{}
+
+		for _, pack := range packsAsStrings {
+			packAmt, err := strconv.Atoi(pack)
+			if err != nil {
+				panic(err)
+			}
+			availablePacks = append(availablePacks, packAmt)
 		}
 	}
 
+	fmt.Fprintln(w, "Welcome to the stock packer: ")
+	fmt.Fprint(w, "Available pack sizes: ")
+
+	lastPack := len(availablePacks) - 1
+
+	for i, pack := range availablePacks {
+		fmt.Fprint(w, pack)
+		if i < lastPack {
+			fmt.Fprint(w, ", ")
+		}
+	}
+
+	fmt.Fprintln(w)
+
+	// string to int
+	qty, err := strconv.Atoi(qtyStr)
+	if err != nil {
+		panic(err)
+	}
+	packing := packItems(availablePacks, qty)
+
+	fmt.Fprintf(w, "Quantity: %d\n", qty)
+
+	for _, entry := range packing {
+		fmt.Fprintf(w, "%d x %d items\n", entry.quantity, entry.pack_volume)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the stock packer")
+	fmt.Fprintln(w, "Go to /pack?quantity=1234 and change quantity to equal the amount of stock you want to manage")
+	fmt.Fprintln(w, "To change pack sizes set the packs query param: /pack?quantity=399&packs=100,75,50,20,10")
+	fmt.Fprintln(w, "Packs must be passed in size order")
+
+}
+
+func main() {
+	http.HandleFunc("/pack", packHandler)
+	http.HandleFunc("/", indexHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
